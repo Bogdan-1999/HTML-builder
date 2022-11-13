c45d5d1beba0c05dcf3e65e120b0c9b772f74451
Sivirinov Bogdan <sibiryuk@mail.ru>
2022-11-13 22:39:25 +0700

    feat: add write solution for 1 task

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,6 +1,8 @@
 const fs = require('fs');
-const path = require('path');
 
-fs.readFile(path.join(__dirname, 'text.txt'), 'utf8', (error, data) => {
-    console.log(data);
-});
+
+const readText = fs.createReadStream(__dirname + '/text.txt', 'utf-8');
+
+readText.on('data', function (chunk) {
+    console.log(chunk);
+})
